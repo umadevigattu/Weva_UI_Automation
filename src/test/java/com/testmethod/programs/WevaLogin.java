ede468eb8b9159c2aed924833e7a319188bb3506
gattumadhu <66872439+gattumadhu@users.noreply.github.com>
2023-11-18 16:56:30 +0530

    Syso added

--- src/test/java/com/testmethod/programs/WevaLogin.java
+++ src/test/java/com/testmethod/programs/WevaLogin.java
@@ -22,6 +22,7 @@ public class WevaLogin extends BaseTest {
 	public void LoginTest() throws Exception {
 
 		wevaUtils.WevaLogin_muliple(seleniumUtilities);
+		System.out.println("Test Passed");
 
 	}
 
