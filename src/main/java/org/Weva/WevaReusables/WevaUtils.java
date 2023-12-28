6f07c4f7724e3651fd51dc7b490738cbc564172f
gattumadhu <66872439+gattumadhu@users.noreply.github.com>
2023-12-28 21:32:38 +0530

    added

--- src/main/java/org/Weva/WevaReusables/WevaUtils.java
+++ src/main/java/org/Weva/WevaReusables/WevaUtils.java
@@ -22,10 +22,13 @@ public class WevaUtils extends BaseTest {
 	public void WevaLogin(SeleniumUtilities seleniumUtilities) throws Exception {
 		extentTest.info("Enter valid username as " + properties.getProperty("username"));
 		seleniumUtilities.EnterText(WevaLoginPage.getUsername(), LocatorType.xpath, properties.getProperty("username"));
+		extentTest.info("User Entered valid username as " + properties.getProperty("username"));
 		extentTest.info("Enter Valid Password as " + properties.getProperty("password"));
 		seleniumUtilities.EnterText(WevaLoginPage.getPassword(), LocatorType.xpath, properties.getProperty("password"));
+		extentTest.info("User Entered Valid Password as " + properties.getProperty("password"));
 		extentTest.info("Click on Sign in button");
 		seleniumUtilities.Click(WevaLoginPage.getSubmit(), LocatorType.xpath);
+		extentTest.info("User Clicked on Sign in button");
 
 		Thread.sleep(5000);
 
@@ -44,14 +47,18 @@ public class WevaUtils extends BaseTest {
 
 			Row row = sheet.getRow(rownum);
 			username = row.getCell(0).getStringCellValue();
-			System.out.println(username);
+			extentTest.info("username is " + row.getCell(0).getStringCellValue());			
 			password = row.getCell(1).getStringCellValue();
-			System.out.println(password);
+			extentTest.info("Password is " + row.getCell(1).getStringCellValue());
 			seleniumUtilities.EnterText(WevaLoginPage.getUsername(), LocatorType.xpath, username);
+			extentTest.info("User Entered Username as " + row.getCell(0).getStringCellValue());
 			seleniumUtilities.EnterText(WevaLoginPage.getPassword(), LocatorType.xpath, password);
+			extentTest.info("User Entered Password as " + row.getCell(1).getStringCellValue());
 			seleniumUtilities.Click(WevaLoginPage.getSubmit(), LocatorType.xpath);
+			extentTest.info("User Clicked on Submit");
 			Thread.sleep(5000);
 			seleniumUtilities.CloseWindow();
+			extentTest.info("User Logged in Successfully");
 		}
 	}
 
